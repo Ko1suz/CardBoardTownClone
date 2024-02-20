8445723ce76340d637aec7da500e06ecdf6acff3
Ko1suz <rooooocer@gmail.com>
2024-02-20 03:31:43 +0300

    Update
    Generic Grid

--- Assets/GridSystem/Scripts/Testing.cs
+++ Assets/GridSystem/Scripts/Testing.cs
@@ -2,35 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using CodeMonkey.Utils;
+using static UnityEngine.Rendering.DebugUI;
+using UnityEngine.UIElements;
 
 public class Testing : MonoBehaviour
 {
     [SerializeField] private HeatMapVisual heatMapVisual;
     [SerializeField] private HeatMapBoolVisual heatMapBoolVisual;
+    [SerializeField] private HeatMapGenericVisual heatMapGenericVisual;
     private Grid<HeatMapGridObject> grid;
+    private Grid<StringGridObject> stringGrid;
     private void Start()
     {
-        grid = new Grid<HeatMapGridObject>
-            (10, 10, 10f, new Vector3(-50, -50), (Grid<HeatMapGridObject> g, int x, int y) => new HeatMapGridObject(g, x, y), false);
+        //grid = new Grid<HeatMapGridObject>(10, 10, 10f, new Vector3(-50, -50), (Grid<HeatMapGridObject> g, int x, int y) => new HeatMapGridObject(g, x, y), false);
+        stringGrid = new Grid<StringGridObject>(10, 10, 10f, new Vector3(-50, -50), (Grid<StringGridObject> g, int x, int y) => new StringGridObject(g, x, y), false);
         //heatMapVisual.SetGrid(grid);
-        //heatMapBoolVisual.SetGrid(grid);
+        //heatMapBoolVisual.SetGrid(grid);  
+        //heatMapGenericVisual.SetGrid(grid);
     }
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Vector3 position = UtilsClass.GetMouseWorldPosition();
+        Vector3 position = UtilsClass.GetMouseWorldPosition();
 
-            HeatMapGridObject heatMapGridObject = grid.GetGridObject(position);
-            if (heatMapGridObject != null)
-            {
-                heatMapGridObject.AddValue(5);
-            }
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    HeatMapGridObject heatMapGridObject = grid.GetGridObject(position);
+        //    if (heatMapGridObject != null)
+        //    {
+        //        heatMapGridObject.AddValue(5);
+        //    }
+        //    //grid.SetValue(position, true);
+        //    //grid.AddValue(position, 100, 5, 20);
+        //}
 
-            //grid.SetValue(position, true);
-            //grid.AddValue(position, 100, 5, 20);
-        }
+        if (Input.GetKeyDown(KeyCode.A)) { stringGrid.GetGridObject(position).AddLetter("A"); };
+        if (Input.GetKeyDown(KeyCode.B)) { stringGrid.GetGridObject(position).AddLetter("B"); };
+        if (Input.GetKeyDown(KeyCode.C)) { stringGrid.GetGridObject(position).AddLetter("C"); };
+
+        if (Input.GetKeyDown(KeyCode.Alpha1)) { stringGrid.GetGridObject(position).AddNumber("1"); };
+        if (Input.GetKeyDown(KeyCode.Alpha2)) { stringGrid.GetGridObject(position).AddNumber("2"); };
+        if (Input.GetKeyDown(KeyCode.Alpha3)) { stringGrid.GetGridObject(position).AddNumber("3"); };
     }
 }
 
@@ -64,5 +76,41 @@ public class HeatMapGridObject
     public override string ToString()
     {
         return value.ToString();
+    }
+}
+
+public class StringGridObject
+{
+    private Grid<StringGridObject> grid;
+    public int x;
+    public int y;
+
+
+    private string letters;
+    private string numbers;
+
+    public StringGridObject(Grid<StringGridObject> grid, int x, int y)
+    {
+        this.grid = grid;
+        this.x = x;
+        this.y = y;
+        letters = "";
+        numbers = "";
+    }
+    public void AddNumber(string number)
+    {
+        numbers += number;
+        grid.TriggerGridObjectChanged(x, y);
+    }
+
+    public void AddLetter(string letter)
+    {
+        letters += letter;
+        grid.TriggerGridObjectChanged(x, y);
+    }
+
+    public override string ToString()
+    {
+        return letters + "\n" + numbers;
     }
 }
