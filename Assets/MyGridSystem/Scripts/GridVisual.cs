1d97498256ff8182e8fce8f7ae2f707c973f4d3f
Ko1suz <rooooocer@gmail.com>
2024-03-21 15:29:57 +0300

    update
    cdmnky sistmeni tekrar et

--- Assets/MyGridSystem/Scripts/GridVisual.cs
+++ Assets/MyGridSystem/Scripts/GridVisual.cs
@@ -1,22 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
-[RequireComponent(typeof(LineRenderer))]
+//[RequireComponent(typeof(LineRenderer))]
 public class GridVisual : GridObject
 {
     LineRenderer _lineRenderer;
+    public bool createGridVisual = false;
+    public Material lineMat;
     // Start is called before the first frame update
     void Start()
     {
-        _lineRenderer = GetComponent<LineRenderer>();
-        _lineRenderer.positionCount = _positions.Length;
-        _lineRenderer.SetPositions(_positions);
+        //_lineRenderer = GetComponent<LineRenderer>();
+        //_lineRenderer.positionCount = _positions.Length;
+        //_lineRenderer.SetPositions(_positions);
+        CreateGrids(createGridVisual);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void CreateGrids(bool createGrid)
+    {
+        if (createGrid)
+        {
+            this.gameObject.AddComponent<LineRenderer>();
+            _lineRenderer = GetComponent<LineRenderer>();
+            _lineRenderer.startWidth = 0.1f;
+            _lineRenderer.endWidth = 0.1f;
+            _lineRenderer.useWorldSpace = false;
+            _lineRenderer.positionCount = _positions.Length;
+            _lineRenderer.SetPositions(_positions);
+
+            Material cloneMat = Instantiate(lineMat);
+            _lineRenderer.material = cloneMat;
+        }
     }
 }
--- Assets/MyGridSystem/Scripts/My_XZ_Grid.cs
+++ Assets/MyGridSystem/Scripts/My_XZ_Grid.cs
@@ -12,42 +12,50 @@ public class My_XZ_Grid : MonoBehaviour
     [SerializeField] private int z_axis_length;
     [SerializeField] private float cellSize;
     [SerializeField] private Vector3 originPosition;
-    [SerializeField] private GameObject[,] gridArray;
+    [SerializeField] private GameObject[,,] gridArray;
 
     private void Start()
     {
-        CreateGrid(gridPrefab, x_axis_length, z_axis_length, cellSize, originPosition);
+        CreateGrid(gridPrefab, x_axis_length, y_axis_length, z_axis_length, cellSize, originPosition);
     }
-    public void CreateGrid(GameObject gridPrefab, int x_axis_lenght, int z_axis_length, float cellSize, Vector3 originPosition)
+    public void CreateGrid(GameObject gridPrefab, int x_axis_lenght, int y_axis_lenght, int z_axis_length, float cellSize, Vector3 originPosition)
     {
         this.gridPrefab = gridPrefab;
         this.x_axis_length = x_axis_lenght;
+        this.y_axis_length = y_axis_lenght;
         this.z_axis_length = z_axis_length;
         this.cellSize = cellSize;
         this.originPosition = originPosition;
 
-        gridArray = new GameObject[x_axis_length, z_axis_length];
-        for (int x = 0; x < gridArray.GetLength(0); x++)
+        gridArray = new GameObject[x_axis_length,y_axis_lenght, z_axis_length];
+        for (int y = 0; y < gridArray.GetLength(2); y++)
         {
-            for (int z = 0; z < gridArray.GetLength(1); z++)
+            for (int x = 0; x < gridArray.GetLength(0); x++)
             {
-                GameObject cloneGrid = Instantiate(this.gridPrefab);
-                cloneGrid.gameObject.SetActive(false);
-                gridArray[x, z] = cloneGrid;
-                cloneGrid.transform.parent = this.transform;
-                if (z%2 == 0 )
+                for (int z = 0; z < gridArray.GetLength(1); z++)
                 {
-                    cloneGrid.GetComponent<GridObject>().Shape = GridObject._shape.Square;
-                    cloneGrid.GetComponent<GridObject>().GridSize = 1 * cellSize;
-                    cloneGrid.transform.localPosition = new Vector3(x * 4, 0, z * 2);
+                    GameObject cloneGrid = Instantiate(this.gridPrefab);
+                    cloneGrid.gameObject.SetActive(false);
+                    gridArray[x, y, z] = cloneGrid;
+                    cloneGrid.transform.parent = this.transform;
+                    if (z % 2 == 0)
+                    {
+                        cloneGrid.GetComponent<GridObject>().Shape = GridObject._shape.Square;
+                        cloneGrid.GetComponent<GridObject>().GridSize = 0.9f * cellSize;
+                        cloneGrid.transform.localPosition = new Vector3(x * 4, y * 5, z * 2);
+                    }
+                    else
+                    {
+                        cloneGrid.GetComponent<GridObject>().Shape = GridObject._shape.Octagon;
+                        cloneGrid.GetComponent<GridObject>().GridSize = 1.8f * cellSize;
+                        cloneGrid.transform.localPosition = new Vector3((x * 4) + 2, y * 5, (z * 2));
+                    }
+                    if (y == 0)
+                    {
+                        cloneGrid.GetComponent<GridVisual>().createGridVisual = true;
+                    }
+                    cloneGrid.SetActive(true);
                 }
-                else
-                {
-                    cloneGrid.GetComponent<GridObject>().Shape = GridObject._shape.Octagon;
-                    cloneGrid.GetComponent<GridObject>().GridSize = 2 * cellSize;
-                    cloneGrid.transform.localPosition = new Vector3((x * 4) + 2, 0, (z *2));
-                }
-                cloneGrid.SetActive(true);
             }
         }
     }
